5d8f5165714bba587cee1f9f43865473bb72873b
Maxim Devaev <mdevaev@gmail.com>
2025-06-11 01:21:15 +0300

    moved to kde6 and wayland

--- .kdekillshadow.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# https://forum.kde.org/viewtopic.php?f=285&t=121592
-for WID in $(xwininfo -root -tree | sed '/"Plasma": ("plasmashell" "plasmashell")/!d; s/^  *\([^ ]*\) .*/\1/g'); do
-	xprop -id $WID -remove _KDE_NET_WM_SHADOW
-done
